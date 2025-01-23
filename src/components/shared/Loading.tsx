88b8ade45c61cef5a6bd86f0cdfba93337343b7d
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2025-01-23 15:02:53 +0000

    cors

--- src/components/shared/Loading.tsx
+++ src/components/shared/Loading.tsx
@@ -3,7 +3,12 @@ import loadingAnimation from "../../assets/loading.json";
 const Loading = () => {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
-			<Lottie className="h-40 w-40" animationData={loadingAnimation} />;
+			<Lottie
+				autoplay
+				loop
+				className="h-40 w-40"
+				animationData={loadingAnimation}
+			/>
 		</div>
 	);
 };
